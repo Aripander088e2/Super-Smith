7a80c6b13cecd97e8ea74d556e465c4163edd75d
Jake O'Rielly <jake.orielly@gmail.com>
2019-10-01 20:38:26 -0400

    Expaned manufacturing

--- main.js
+++ main.js
@@ -10,8 +10,11 @@ var produceKeyFuncs = {
     'k':() => {coalToFurnace(1)},
     'K':() => {coalToFurnace(2)},
     'l':copperToFurnace,
-    'z':() => {manufacture(copper_wire)},
-    'b':() => {manufacture(simple_circuitboard)}
+    'z':() => {manufacture(iron_plate)},
+    'x':() => {manufacture(copper_wire)},
+    'b':() => {manufacture(iron_bulkhead)},
+    'n':() => {manufacture(simple_circuit_board)},
+    'm':() => {manufacture(small_engine)},
 };
 
 var sellKeyFuncs = {
@@ -63,11 +66,14 @@ var copper_ore = {name:'copper_ore',val:5}
 var iron_bar = {name:'iron_bar',val:12};
 var copper_bar = {name:'copper_bar',val:25};
 var copper_wire = {name:'copper_wire',val:7,recipe:{copper_bar:1},output:5};
-var simple_circuitboard = {name:'simple_circuit_board',val:225,recipe:{copper_wire:15,iron_bar:2}};
+var iron_plate = {name:'iron_plate',val: 19, recipe:{iron_bar:1}};
+var iron_bulkhead = {name:'iron_bulkhead',val:200,recipe:{iron_plate:5}};
+var simple_circuit_board = {name:'simple_circuit_board',val:275,recipe:{copper_wire:15,iron_bar:2}};
+var small_engine = {name:'small_engine',val:325,recipe:{copper_wire:10,copper_bar:2,iron_bar:3}};
 
-var items = {iron_ore:iron_ore,coal:coal,iron_bar:iron_bar,
+var items = {iron_ore:iron_ore,coal:coal,iron_bar:iron_bar,iron_plate:iron_plate,iron_bulkhead:iron_bulkhead,
     copper_ore:copper_ore,copper_bar:copper_bar,copper_wire:copper_wire,
-    simple_circuitboard:simple_circuitboard};
+    simple_circuit_board:simple_circuit_board,small_engine:small_engine};
 
 var smeltCooldown = [0,0];
 var maxSmeltCooldown = 60;
@@ -115,8 +121,9 @@ function copperToFurnace() {
 }
 
 function manufacture(item) {
+    let amount = (item.output != undefined ? item.output : 1);
     if (removeRecipe(item.recipe,'player'))
-        addItem(item,'player',item.output);
+        addItem(item,'player',amount);
 }
 
 setInterval(gameTick,50);
